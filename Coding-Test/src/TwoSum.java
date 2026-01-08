eb78bccdc26acd68b79dce2bde26ef007268a554
sangwuny <144337247+sangwuny@users.noreply.github.com>
2026-01-08 13:05:54 +0900

    add extra solution for two sum using hash table

--- Coding-Test/src/TwoSum.java
+++ Coding-Test/src/TwoSum.java
@@ -10,16 +10,30 @@ import java.util.*;
  */
 
 public class TwoSum {
-    class Solution {
-        public int[] twoSum(int[] nums, int target) {
-            for(int i=0;i<nums.length;i++){
-                int temp = target - nums[i];
-                for(int j=i+1;j<nums.length;j++){
-                    if(nums[j]==temp) return new int[]{i,j};
-                }
-            }
+//        기존 나의 풀이 방식
+//    public int[] twoSum(int[] nums, int target) {
+//        for (int i = 0; i < nums.length; i++) {
+//            int temp = target - nums[i];
+//            for (int j = i + 1; j < nums.length; j++) {
+//                if (nums[j] == temp) return new int[]{i, j};
+//            }
+//        }
+//
+//        return null;
+//    }
 
-            return null;
+    //    Hash Table을 이용한 방식
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int need = target - nums[i];
+
+            if(map.containsKey(need)) return new int[]{map.get(need), i};
+
+            map.put(nums[i], i);
         }
+
+        return new int[]{};
     }
 }
